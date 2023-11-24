9a596928a8b2552c87b06e9e224f62abe1b1e99b
Mattéo <mfauchon@corp.free.fr>
2023-11-24 14:55:31 +0100

    Initial Commit

---
+++ Bored/BoredApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoredApp.swift
+//  Bored
+//
+//  Created by Mattéo Fauchon  on 24/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct BoredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
